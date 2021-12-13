580f93fd7b0b9f37d9f4485e6a9aae60c712bb5b
Betoml5 <betoml5@hotmail.com>
2021-12-13 00:22:23 -0600

    Fix bestsellers endpoint

--- controllers/Book.js
+++ controllers/Book.js
@@ -1,36 +1,24 @@
 const { config } = require("../config");
 const axios = require("axios");
-const response = require("../network/response");
+const responseHTTP = require("../network/response");
 
 const controller = {
-  bestsellers: async (req, res) => {
-    try {
-      const bestSellers = await axios({
-        method: "GET",
-        url: `${config.nyApi}/best-sellers/history.json?offset=20&api-key=${config.nykey}`,
-      });
-      let books = bestSellers.data.results;
-      let isbns = books.map((book) => book.isbns);
-      let filterIsbns = isbns.map((item) => item.map((item) => item.isbn10));
-      const isbnsArray = [];
-      for (let i = 0; i < filterIsbns.length; i++) {
-        for (let j = 0; j < filterIsbns[i].length; j++) {
-          isbnsArray.push(filterIsbns[i][j]);
+    bestSellers: async (req, res, next) => {
+        try {
+            const response = await axios.get(
+                `${config.nyApi}/lists/overview.json?api-key=gcWBKCOX8oLT4s5XXW5Iq0CXPRAC1IGH`
+            );
+
+            return responseHTTP.success(
+                req,
+                res,
+                response.data.results.lists,
+                200
+            );
+        } catch (error) {
+            return responseHTTP.error(req, res, error, 500);
         }
-      }
-      const results = [];
-      for (let i = 0; i < isbnsArray.length; i++) {
-        const response = await axios.get(
-          `${config.openlibraryApi}?bibkeys=ISBN:${isbnsArray[i]}&jscmd=details&format=json`
-        );
-        results.push(response.data);
-      }
-      return response.success(req, res, results, 200);
-    } catch (error) {
-      console.log(error.message);
-      return response.error(req, res, error, 500);
-    }
-  },
+    },
 };
 
 module.exports = controller;
--- routes/Book.js
+++ routes/Book.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/Book");
 
-router.get("/best", controller.bestsellers);
+router.get("/best", controller.bestSellers);
 
 module.exports = router;
