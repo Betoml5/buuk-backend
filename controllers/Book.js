aa3e473a47e323f2cc0185c99de21689e5ea87a3
Betoml5 <betoml5@hotmail.com>
2021-12-14 15:55:33 -0600

    Add search book endpoint

--- controllers/Book.js
+++ controllers/Book.js
@@ -4,6 +4,8 @@ const responseHTTP = require("../network/response");
 
 const controller = {
     bestSellers: async (req, res) => {
+        //Nota importante. La API de NYTIMES el path /overview devuelve imagen.
+        //Es por eso que ya no hacemos 2 peticiones. Ya no utilizamos a OPENLIBRARY
         try {
             setTimeout(async () => {
                 const response = await axios.get(
@@ -21,14 +23,30 @@ const controller = {
             return responseHTTP.error(req, res, error, 500);
         }
     },
-    // searchBook: async (req,res) => {
-    //     const { q } = req.query;
-    //     try {
-    //         const
-    //     } catch (error) {
+    searchBook: async (req, res) => {
+        const { title } = req.query;
+        try {
+            const {
+                data: { docs },
+            } = await axios.get(
+                `${config.openlibraryApi}/search.json?title=${title}`
+            );
 
-    //     }
-    // }
+            const firstDoc = docs[0] || [];
+            const bookCover = `https://covers.openlibrary.org/b/lccn/${firstDoc.cover_i}-L.jpg`;
+
+            const book = {
+                title: firstDoc.title,
+                subtitle: firstDoc.subtitle | " ",
+                cover: bookCover,
+                author: firstDoc.author_name,
+            };
+
+            return responseHTTP.success(req, res, book, 200);
+        } catch (error) {
+            return responseHTTP.error(req, res, error, 500);
+        }
+    },
 };
 
 module.exports = controller;
--- routes/Book.js
+++ routes/Book.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../controllers/Book");
 
 router.get("/best", controller.bestSellers);
+router.get("/search", controller.searchBook);
 
 module.exports = router;
