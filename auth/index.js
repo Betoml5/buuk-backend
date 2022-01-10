c923c348edd35cf9724f000632aec2d5625abc9b
Betoml5 <betoml5@hotmail.com>
2022-01-10 12:53:08 -0600

    Remove facebook strategy add middleware to check jwt

--- auth/index.js
+++ auth/index.js
@@ -3,7 +3,6 @@ const { config } = require("../config");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
 const JWTStrategy = require("passport-jwt").Strategy;
 const ExtractJWT = require("passport-jwt").ExtractJwt;
 const User = require("../models/User");
@@ -35,25 +34,6 @@ passport.use(
     )
 );
 
-passport.use(
-    new FacebookStrategy(
-        {
-            clientID: "218559347137366",
-            clientSecret: "88bcf86f7152ac07107ccb96f7fb089d",
-            callbackURL: "http://localhost:3080/api/v1/auth/facebook/callback",
-            profileFields: ["id", "name", "picture", "email"],
-        },
-        function (accessToken, refreshToken, profile, done) {
-            // console.log(accessToken);
-            // console.log(refreshToken);
-            // console.log(profile.name);
-
-            console.log(profile._json);
-            done(null, profile);
-        }
-    )
-);
-
 // Con este middleware, vamos a verificar el token en cada peticion.
 // Tenemos que ponerlo en cada ruta, para valdiar
 passport.use(
@@ -76,10 +56,11 @@ passport.use(
     )
 );
 
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
+//Esto es para que funcione la estrategia de Facebook
+// passport.serializeUser((user, done) => {
+//     done(null, user);
+// });
 
-passport.deserializeUser((user, done) => {
-    done(null, user);
-});
+// passport.deserializeUser((user, done) => {
+//     done(null, user);
+// });
