3b5e250b4d1483b38adaa0ef7d8a31a90c6c74da
Betoml5 <betoml5@hotmail.com>
2022-02-12 13:21:35 -0600

    Dummy change

--- controllers/User.js
+++ controllers/User.js
@@ -148,7 +148,6 @@ const controller = {
             user.save({ new: true });
             return responseHTTP.success(req, res, user, 200);
         } catch (error) {
-            console.log(error);
             return responseHTTP.error(req, res, error, 500);
         }
     },
